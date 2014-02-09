02b4cd1b7d23fc276d5def9d5b09a018677cebf0
aaz <akbar.zamir@gmail.com>
2014-02-09 10:59:39 +0000

    Generalised sequence analysis, back to pure funcs.

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/math.combinatorics "0.0.7"]
                  [incanter/incanter-core "1.5.5-SNAPSHOT"]
                  [incanter/incanter-charts "1.5.5-SNAPSHOT"]])
--- src/sfi_chaos/unit03.clj
+++ src/sfi_chaos/unit03.clj
@@ -1,25 +1,31 @@
-(ns sfi-chaos.unit03)
+(ns sfi-chaos.unit03
+  (:use (clojure.math combinatorics)))
 
-(defn logistic-eqn
+(defn logistic-map
   "Takes growth factor r and current population x. x is a proportion of the annihilation population, between 0 and 1"
   [r x]
   (* r x (- 1 x)))
 
 (defn symbolise
   "Using break point b express the sequence s in terms of values being 'Left' or 'Right' of b"
-  [b s]
-  (map #(if (< % b) :L :R) s))
+  ([s] (symbolise 0.5 s))
+  ([b s] (map #(if (< % b) :L :R) s)))
 
-(defn symbol-pattern-freq
-  "Count occurences of a given symbol pattern p in the overall sequence s"
-  [p s]
-  (let [symbol-seqs (partition (count p) 1 s)]
-    (count (filter #(= % p) symbol-seqs))))
+(defn count-patterns
+  "s - partitioned sequence, p - patterns"
+  [s p]
+  (loop [parts s
+         tally (zipmap p (repeat (count p) 0))]
+    (cond
+     (empty? parts) tally
+     (not (contains? tally (first parts))) (recur (rest parts) tally)
+     :else (recur (rest parts) (assoc tally (first parts)
+                                      (inc (tally (first parts))))))))
 
-;; Generalise for arbitrary pattern length - use a macro?
-(defn print-sequence-summary
-  "Display counts for each symbol pattern"
-  [s]
-  (doseq [first '(:L :R) second '(:L :R)]
-    (println (symbol-pattern-freq (list first second) s)
-             (list first second))))
+(defn analyse-symbol-sequence
+  "ss - symbol sequence, si - symbols of interest, l - pattern length"
+  [ss si l]
+  ;; Partition sequence; Create patterns
+  (let [parts (partition l 1 ss)
+        patterns (selections si l)]
+    (count-patterns parts patterns)))
