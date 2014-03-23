41aed768a21619fad51d5afd19bb0758f5aafe95
aaz <akbar.zamir@gmail.com>
2014-03-23 19:57:50 +0000

    Plot sample orbits.

--- src/sfi_chaos/core.clj
+++ src/sfi_chaos/core.clj
@@ -1,6 +1,23 @@
-(ns sfi-chaos.core)
+(ns sfi-chaos.core
+  (:use [sfi-chaos.unit03]
+        [incanter core charts]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def lm20 (partial logistic-map 2.0))
+(def lm30 (partial logistic-map 3.0))
+(def lm35 (partial logistic-map 3.5))
+(def lm357 (partial logistic-map 3.57))
+(def lm40 (partial logistic-map 4.0))
+
+(def NUMBER-TO-PLOT 40)
+(def NUMBER-TO-SKIP 1400)
+
+(def series-1 (take NUMBER-TO-PLOT (drop NUMBER-TO-SKIP (iterate lm40 0.2))))
+(def series-2 (take NUMBER-TO-PLOT (drop NUMBER-TO-SKIP (iterate lm40 0.200000001))))
+(def iterations (range 0 NUMBER-TO-PLOT))
+
+(def plot (line-chart iterations series-1 :legend true :series-label "s1"))
+(set-stroke plot :series 0 :width 2.5)
+(add-categories plot iterations series-2 :series-label "s2")
+(set-stroke plot :series 1 :width 2.5)
+
+(view plot)
